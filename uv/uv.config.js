406fbc8c589a043a601d22eab9d6554f121ea324
PaxLeis <lpaxton0101@crprairie.org>
2025-10-06 11:33:15 -0500

    Add files via upload

---
+++ uv/uv.config.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+	prefix: "/NebuliOS/p/ultraviolet/ultraviolet/",
+	bare: "https://meximathurbanned.global.ssl.fastly.net/tspmo/",
+	encodeUrl: Ultraviolet.codec.xor.encode,
+	decodeUrl: Ultraviolet.codec.xor.decode,
+	handler: "/NebuliOS/p/ultraviolet/uv/uv.handler.js",
+	bundle: "/NebuliOS/p/ultraviolet/uv/uv.bundle.js",
+	config: "/NebuliOS/p/ultraviolet/uv/uv.config.js",
+	sw: "/NebuliOS/p/ultraviolet/uv/uv.sw.js",
+};
